7429068e3295c1e1eff0ca53b82f162b997138b4
Endre Ujhelyi <endreujhelyi@gmail.com>
2017-01-11 10:45:10 +0100

    validator function works

---
+++ validator.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const niceWords = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
+
+
+function validator(email, number, feedback) {
+  let validEmail = false;
+  let validNumber = false;
+  let validFeedback = true;
+  let validate = false;
+
+
+  for (let i = 0; i < email.length; i++) {
+    if (email[i] === '@') {
+      validEmail = true;
+    }
+  }
+
+  if (number > 0 && number <= 10 ) {
+    validNumber = true;
+  }
+
+  let kindWords = 0;
+  let userWords = feedback.split(' ');
+
+  for (let i = 0; i < userWords.length; i++) {
+    niceWords.map(function(word) {
+      if (word == userWords[i]) {
+        kindWords++;
+      }
+    })
+  }
+
+  if (validEmail && validNumber && kindWords >= 3) {
+    validate = true;
+  }
+  return validate;
+};
+
+module.exports = validator;
